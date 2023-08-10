3a28ce394046a5e10d71b4ce2fffd258b078830e
Danielle <136658130+Smashhedo@users.noreply.github.com>
2023-08-09 19:30:39 -0500

    Complete

---
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/area.java
@@ -0,0 +1,14 @@
+package org.launchcode;
+
+import java.util.Scanner;
+
+public class area {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a radius.");
+        double radius = input.nextDouble();
+        System.out.println("The area of a circle with the radius of " + radius + ", is " + circle.getArea(radius));
+
+    }
+
+}
